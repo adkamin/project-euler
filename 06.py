880e1b2c8f7dc18724245136e6d180e505c957af
adkamin <minichovaa@gmail.com>
2022-09-26 14:13:21 +0200

    ename files

--- 06.py
+++ 06.py
@@ -2,7 +2,6 @@
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
-
 def find_difference(n):
     sum = sum_of_squares = 0
     for i in range(1,n+1):
